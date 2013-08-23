95b69455d1522805b0a089f033dea342a82c1188
filipecaixeta@gmail.com <filipecaixeta@gmail.com@4dad75bd-073e-543b-f220-287fb07f53da>
2013-08-23 23:48:57 +0000

    codigo do mysql

---
+++ db_mysql.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 23 11:36:30 2013
+
+@author: filipe
+"""
+
+from MySQLdb import Connect
+
+class Connect_MySQL:
+
+    def __init__(self,host,user,passwd):
+        self.conn = Connect(host, user, passwd)
+        self.curs = self.conn.cursor()
+        self.curs.execute('use controledeacesso')
+
+    def cria_tabelas(self):
+        self.curs.execute("CREATE TABLE funcionarios (id_funcionario INT NOT NULL AUTO_INCREMENT PRIMARY KEY, matricula INT NOT NULL', rfid varchar(20))")
+        self.curs.execute("CREATE TABLE horarios (id_horario INT NOT NULL AUTO_INCREMENT PRIMARY KEY, id_funcionario INT NOT NULL, dia_da_semana INT NOT NULL, hora TIME NOT NULL)")
+        self.curs.execute("CREATE TABLE pontos (id_ponto INT NOT NULL AUTO_INCREMENT PRIMARY KEY, id_funcionario INT NOT NULL, id_horario INT NOT NULL, horario_entrada DATETIME NOT NULL, horario_saida DATETIME, atraso_entrada TIME, atraso_saida TIME, presenca INT)")
+
+    def cria_funcionario(self,nome,rfid,horario,)
+
+'''
+    def select(self):
+        curs.execute('select * from people')
+        for row in curs.fetchall():
+            print(row)
+        curs.execute('select * from people where name = %s', ('Bob',))
+        print(curs.description)
+        colnames = [desc[0] for desc in curs.description]
+        while True:
+            print('-' * 30)
+            row = curs.fetchone()
+            if not row: break
+            for (name, value) in zip(colnames, row):
+                print('%s => %s' % (name, value))
+        conn.commit()
+'''
