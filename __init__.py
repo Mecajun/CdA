4759f537b068795bffa3b467fc734668c6a89592
filipecaixeta@gmail.com <filipecaixeta@gmail.com@4dad75bd-073e-543b-f220-287fb07f53da>
2013-08-27 02:23:59 +0000

    adicionado thread

--- __init__.py
+++ __init__.py
@@ -8,13 +8,25 @@ Created on Mon Aug 26 00:27:34 2013
 import model_mysql
 import gui
 import wx
+import threading
+import time
 
 
-
+class Controller_main(threading.Thread):
+    def __init__(self):
+        super(Controller_main, self).__init__()
+    
+    def run(self):
+        while True:
+            time.sleep(1)
+            print time.asctime()
+    
 if __name__ == "__main__":
     app = wx.PySimpleApp(0)
     wx.InitAllImageHandlers()
     Frame1 = gui.MainFrame(None, -1, "")
     app.SetTopWindow(Frame1)
+    a=Controller_main()
+    a.start()    
     Frame1.Show()
     app.MainLoop()
