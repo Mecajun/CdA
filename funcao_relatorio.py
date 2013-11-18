96aaa8be6d33984d84cc0c80121a6ed545a9d188
lucasvillela.unb@gmail.com <lucasvillela.unb@gmail.com@4dad75bd-073e-543b-f220-287fb07f53da>
2013-11-18 12:21:51 +0000

    funcao_relatorio.py contem as duas funcoes de realtorio

--- funcao_relatorio.py
+++ funcao_relatorio.py
@@ -1,28 +1,59 @@
 import datetime
 import csv
 
-def relatorio(dados):
-    extensao='.csv'
-    arquivo=datetime.datetime.now().strftime('%d_%m_%Y')
-    nome_arquivo=arquivo+extensao
-    saida=csv.writer(file(nome_arquivo, 'w'))
-    for linha in dados:
-        saida.writerow(linha)      
+def gerar_Relatorio_Porta(dados):
+	vet=[]
+	for elem in dados:
+		vet2=[]
+		vet2.append(elem[0])
+		vet2.append(elem[1])
+		tempo = elem[2].strftime('%d/%m/%Y - %H:%M')
+		vet2.append(tempo)
+		vet.append(vet2)
 
-b=((u'a', u'42', datetime.datetime(2013, 11, 5, 16, 47)), (u'b', u'24', datetime.datetime(2013, 11, 5, 16, 47)), (u'b', u'24', datetime.datetime(2013, 11, 5, 16, 47)), (u'um', u'1', datetime.datetime(2013, 11, 5, 17, 14)), (u'b', u'24', datetime.datetime(2013, 11, 5, 17, 25)), (u'a', u'42', datetime.datetime(2013, 11, 5, 17, 25)))
-a=b[0]
-vet=[]
-for elem in b:
-	vet2=[]
-	vet2.append(elem[0])
-	vet2.append(elem[1])
-	tempo = elem[2].strftime('%d/%m/%Y - %H:%M')
-	vet2.append(tempo)
-	vet.append(vet2)
+	extensao='.csv'
+	arquivo=datetime.datetime.now().strftime('log_porta_%d_%m_%Y')
+	nome_arquivo=arquivo+extensao
+	saida=csv.writer(file(nome_arquivo, 'w'))
+	for linha in dados:
+	    saida.writerow(linha)
 
-for i in vet:
-	print i
-	relatorio(i)
+def gerar_Relatorio_Pontos(dados):
+	vet=[]
+	for elem in dados:
+		vet2 = []
+		#nome do usuario
+		vet2.append(elem[0])
+		#matricula
+		vet2.append(elem[1])
+		#hora de entrada
+		hora = elem[2].strftime('%d/%m/%Y - %H:%M')
+		#hora de saida
+		hora1 = elem[3].strftime('%d/%m/%Y - %H:%M')
+		vet2.append(hora)
+		vet2.append(hora1)
+		vet2.append(str(elem[4]))
+		vet2.append(str(elem[5]))
+		if elem[6] == 1:
+			vet2.append('sem atraso')
+		elif elem[6] == -2:
+			vet2.append('nao fechou')
+		elif elem[6] == -3:
+			vet2.append('chegou atrasado')
+		elif elem[6] == -4:
+			vet2.append('saiu com atraso')
+		elif elem[6] == -5:
+			vet2.append('chegou e saiu com atraso')
+		else elem[6] == -1:
+			vet2.append('ponto aberto')
+		vet.append(vet2)
+
+	extensao='.csv'
+	arquivo=datetime.datetime.now().strftime('log_pontos_%d_%m_%Y')
+	nome_arquivo=arquivo+extensao
+	saida=csv.writer(file(nome_arquivo, 'w'))
+	for linha in dados:
+	    saida.writerow(linha)
 
 
 
